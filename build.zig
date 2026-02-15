aef8153d6173cf81d0a27a75fe2f82605b7fc7cc
Pius Arhanbhunde <pjacks419@gmail.com>
2026-02-15 17:17:54 +0100

    Used inline for

--- build.zig
+++ build.zig
@@ -3,46 +3,28 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    // Density altitude calculator
-    const exe = b.addExecutable(.{
-        .name = "density_altitude_calculator",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("calculator/density_altitude_calculator.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
-    });
-    // Flight calcuator
-    const flight_exe = b.addExecutable(.{
-        .name = "flight_calculator",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("calculator/flight_calculator.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
-    });
-    // Turn calcuator
-    const turn_exe = b.addExecutable(.{
-        .name = "turn_calculator",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("calculator/turn_calculator.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
-    });
-    // VNAV calcuator
-    const vnav_exe = b.addExecutable(.{
-        .name = "vnav_calculator",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("calculator/vnav_calculator.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
-    });
-
-    // create executables
-    b.installArtifact(exe);
-    b.installArtifact(flight_exe);
-    b.installArtifact(turn_exe);
-    b.installArtifact(vnav_exe);
+    const src_files = .{
+        "calculator/density_altitude_calculator.zig", 
+        "calculator/flight_calculator.zig", 
+        "calculator/turn_calculator.zig", 
+        "calculator/vnav_calculator.zig"
+    };
+    const exe_files = .{
+        "density_altitude_calculator", 
+        "flight_calculator", 
+        "turn_calculator", 
+        "vnav_calculator"
+    };
+    inline for (0..src_files.len) |i|{
+        // Density altitude calculator
+        const exe = b.addExecutable(.{
+            .name = exe_files[i],
+            .root_module = b.createModule(.{
+                .root_source_file = b.path(src_files[i]),
+                .target = target,
+                .optimize = optimize,
+            }),
+        });
+        b.installArtifact(exe);
+    }
 }
