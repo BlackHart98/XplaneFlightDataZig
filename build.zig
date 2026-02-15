38a645178abff3ec419eccf5791f447f3bd38e56
Pius Arhanbhunde <pjacks419@gmail.com>
2026-02-15 17:47:56 +0100

    fix(): Remove redundant list

--- build.zig
+++ build.zig
@@ -3,24 +3,18 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const src_files = .{
-        "calculator/density_altitude_calculator.zig", 
-        "calculator/flight_calculator.zig", 
-        "calculator/turn_calculator.zig", 
-        "calculator/vnav_calculator.zig"
-    };
     const exe_files = .{
         "density_altitude_calculator", 
         "flight_calculator", 
         "turn_calculator", 
         "vnav_calculator"
     };
-    inline for (0..src_files.len) |i|{
+    inline for (exe_files) |item|{
         // Density altitude calculator
         const exe = b.addExecutable(.{
-            .name = exe_files[i],
+            .name = item,
             .root_module = b.createModule(.{
-                .root_source_file = b.path(src_files[i]),
+                .root_source_file = b.path("calculator/" ++ item ++ ".zig"),
                 .target = target,
                 .optimize = optimize,
             }),
